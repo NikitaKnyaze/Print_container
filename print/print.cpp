373de406995e4e729eb017b787da6231cef01e6c
Knyazev Nikita <nk8222138@gmail.com>
2024-05-26 17:46:51 +0300

    first commit

---
+++ print/print.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+
+template <class T>
+
+void print_container(T& a, T& b)
+{
+	for (; a != b; ++a)
+	
+		std::cout << *a << " ";
+}
+
+int main()
+{
+	std::vector<std::string> my_v{ "one", "two", "three", "four" };
+	std::vector<std::string>::iterator i = my_v.begin();
+	std::vector<std::string>::iterator j = my_v.end();
+	print_container(i, j);
+
+	return 0;
+}
+
